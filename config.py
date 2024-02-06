c298eaecf22b54588c604e52a67053c05346d1c2
landanfp <159138257+landanfp@users.noreply.github.com>
2024-02-06 20:21:37 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,31 +3,11 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH","")
-    # Get these values from my.telegram.org
-    # the download location, where the HTTP Server runs
-    DOWNLOAD_LOCATION = "./DOWNLOADS"
-    # Telegram maximum file upload size
-    MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 4194304000 #2097152000
-    FREE_USER_MAX_FILE_SIZE = 50000000
-    # chunk size that should be used with requests
-    CHUNK_SIZE = int(128)
-    # default thumbnail to be used in the videos
-    # proxy for accessing youtube-dl in GeoRestricted Areas
-    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
-    HTTP_PROXY = ""
-    # maximum message length in Telegram
-    MAX_MESSAGE_LENGTH = 4096
-    # set timeout for subprocess
-    PROCESS_MAX_TIMEOUT = 3600
-    # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-    SESSION_NAME = "UPLOADER-X-BOT"
-    # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    MAX_RESULTS = "50"
-    PREMIUM_USER = os.environ.get("PREMIUM_USER")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5088657122:AAGByZ5PLpv205PaXhxDEZ_JixPIn1OaVnk")
+    API_ID = int(os.environ.get("API_ID", "3335796"))
+    APP_HASH = os.environ.get("APP_HASH","138b992a0e672e8346d8439c3f42ea78")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "VIDEO-EN")
+    OWNER_ID = int(os.environ.get("OWNER_ID", "763990585")
+    SUDO_USERS = os.environ.get("SUDO_USERS","")
+    MONGO_DB = os.environ.get("MONGO_DB", "mongodb+srv://abirhasan2005:abirhasan@cluster0.i6qzp.mongodb.net/cluster0?retryWrites=true&w=majority")
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL, "-1001792962793")
